e0047fef0cd9d73332e01a7bc2ae9e6613b32f35
Yashchenko Illia <illeeardi@gmail.com>
2023-10-17 23:48:28 +0300

    created task 5

---
+++ hw-for-x2
@@ -0,0 +1,5 @@
+// Виводимо 20 чисел, але парними
+
+// for (let i = 0; i <= 20; i = i + 2) {
+//   console.log("One iteration i =", i);
+// }
--- hw-for.js
+++ hw-for.js
@@ -1,3 +1,5 @@
-for (let i = 0; i <= 10; i++) {
-  console.log("One iteration i =", i);
-}
+// Виводимо кожне число до 10 включно з "10"
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log("One iteration i =", i);
+// }
