9fea13a49ed7733765d19c0fa0d657ee4994390d
Yashchenko Illia <illeeardi@gmail.com>
2023-10-18 00:19:31 +0300

    created task number 4

---
+++ hw-while.js
@@ -0,0 +1,8 @@
+// З числа "з клавіатури" до числа 10 або менше
+
+let randomIndex = 123456;
+
+while (randomIndex >= 10) {
+  console.log("Reverse numer less than or equal to 10 =", randomIndex);
+  randomIndex--;
+}
